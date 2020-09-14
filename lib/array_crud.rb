9df7f105b5bcb181f938ca737454a1d862181d16
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-14 23:22:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["doggo", "cat", "frog"]
-  my_array << "lizard"
+  ["doggo", "cat", "frog"]
+  << "lizard"
 end
 
 def add_element_to_start_of_array(array, element)
