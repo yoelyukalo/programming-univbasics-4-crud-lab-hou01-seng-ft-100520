8fb474772c6fb4375f7de50b27e4e400a72e5864
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-14 23:11:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["yoel","lidia","nahum","max"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    arr = ["yoel","lidia","nahum","max"]
+    arr.pop
 end
 
 def remove_element_from_start_of_array(array)
